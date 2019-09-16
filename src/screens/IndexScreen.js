4c49125f57a70f2571650e61699029d88f249b3a
Jason Crawford <daxapps777@gmail.com>
2019-09-16 06:31:54 -0400

    delete blog post

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,19 +1,28 @@
 import React, { useContext } from 'react';
-import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
-	const { state, addBlogPost } = useContext(Context);
+	const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
 	return (
 		<View style={styles.container}>
-			<Text>RN Mobile Blog</Text>
 			<Button title="Add Post" onPress={addBlogPost} />
 			<FlatList
 				data={state}
 				keyExtractor={(blogPost) => blogPost.title}
 				renderItem={({ item }) => {
-					return <Text>{item.title}</Text>;
+					return (
+						<View style={styles.row}>
+							<Text style={styles.title}>
+								{item.title} - {item.id}
+							</Text>
+							<TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+								<Feather style={styles.icon} name="trash" />
+							</TouchableOpacity>
+						</View>
+					);
 				}}
 			/>
 		</View>
@@ -21,11 +30,19 @@ const IndexScreen = () => {
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center'
+	row: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		paddingVertical: 20,
+		paddingHorizontal: 10,
+		borderTopWidth: 1,
+		borderColor: 'gray'
+	},
+	title: {
+		fontSize: 18
+	},
+	icon: {
+		fontSize: 24
 	}
 });
 
