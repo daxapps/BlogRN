e83a0d09c39578a7f9e3b9be10ff6c6d5b3b2b9d
Jason Crawford <daxapps777@gmail.com>
2019-09-16 11:46:21 -0400

    add plus sign to navigate to CreateScreen

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { Context } from '../context/BlogContext';
+// import BlogPostForm from '../components/BlogPostForm';
+
+const CreateScreen = ({ navigation }) => {
+	const { addBlogPost } = useContext(Context);
+
+	return (
+		<Text>CREATE</Text>
+		// <BlogPostForm
+		// 	onSubmit={(title, content) => {
+		// 		addBlogPost(title, content, () => navigation.navigate('Index'));
+		// 	}}
+		// />
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
