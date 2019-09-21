e7c4258f2ef453933d0cbde04f18f5f5b8075bb9
Jason Crawford <daxapps777@gmail.com>
2019-09-21 09:37:19 -0400

    use db.json and ngrok as temp db

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,25 @@
-import React, { useContext } from 'react';
-import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity } from 'react-native';
+import React, { useContext, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = ({ navigation }) => {
-	const { state, addBlogPost, deleteBlogPost } = useContext(Context);
+	const { state, deleteBlogPost, getBlogPosts } = useContext(Context);
+
+	useEffect(() => {
+		getBlogPosts();
+
+		const listener = navigation.addListener('didFocus', () => {
+			getBlogPosts();
+		});
+
+		return () => {
+			listener.remove();
+		};
+	}, []);
 
 	return (
-		<View style={styles.container}>
-			<Button title="Add Post" onPress={addBlogPost} />
+		<View>
 			<FlatList
 				data={state}
 				keyExtractor={(blogPost) => blogPost.title}
